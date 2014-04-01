e0e6f9980075fb05393812fb5a7800e0838efe4a
danielgalassi@gmail.com <danielgalassi@gmail.com@b044f69f-097e-1059-c40c-61760cd1052f>
2014-04-01 00:29:00 +0000

    Better handling of issues when no tests are found.

--- src/org/validator/services/ValidatorService.java
+++ src/org/validator/services/ValidatorService.java
@@ -72,73 +72,83 @@ public class ValidatorService extends HttpServlet {
 			return;
 		}
 
-		//trimming repository file
-		//keeping only selected subject area objects
+		//trimming repository file, keeping only selected subject area objects
 		repository.trim(selectedSubjectArea);
 
 		//validates the repository file can be used
 		if (repository.available()) {
-			//setup a results directory if tests are found
-			boolean testsFound = (getServletContext().getResourcePaths(testCatalogLocation).size() > 0);
-			if (testsFound) {
-
-				resultCatalogLocation = workDirectory + "results" + File.separator;
-
-				//setting up the validator engine with a repository and tests
-				ValidatorEngine engine = new ValidatorEngine();
-				engine.setRepository(repository);
-				engine.setStartTime(startTime);
-				engine.setResultCatalogLocation(resultCatalogLocation);
-
-				//adding tests to the validator engine
-				Set <String> testSuite = getServletContext().getResourcePaths(testCatalogLocation);
-				for (String testCase : testSuite) {
-					engine.addTest(getServletContext().getResourceAsStream(testCase));
-				}
-
-				//it's time to run all tests on this trimmed repository,
-				//save results in that location and time the whole operation
-				if (engine.ready()) {
-					engine.run();
-				}
-
-				//the results page is created
-				InputStream			xsl2html = null;
-				String				resultsFormat = (String) session.getAttribute("resultsFormat");
-				Map<String, String>	stylesheetParams = new HashMap<String, String> ();
-				String 				stylesheet = "";
-				String				errorsOnlyMode = "false";
-				File 				index = new File(resultCatalogLocation + "index.xml");
-
-				//setting up stylesheet parameters
-				stylesheetParams.put("SelectedSubjectArea", selectedSubjectArea);
-				if (resultsFormat.equals("ShowErrorsOnly")) {
-					errorsOnlyMode = "true";
-				}
-				stylesheetParams.put("ShowErrorsOnly", errorsOnlyMode);
-				stylesheetParams.put("SessionFolder", sessionId);
-
-				//creating both HTML pages (Summary and Details views)
-				Vector<String> pages = new Vector<String>();
-				pages.add("Summary");
-				pages.add("Compact");
-				pages.add("Details");
-				for (String page : pages) {
-					stylesheet = viewCatalogLocation + page + ".xsl";
-					xsl2html = getServletContext().getResourceAsStream(stylesheet);
-					XMLUtils.applyStylesheet(index, xsl2html, resultCatalogLocation + page + ".html", stylesheetParams);
-				}
-				//Summary contains links suitable to the Servlet Container environment
-				//No need to include it, it is replaced by Compact in the ZIP 
-				pages.remove("Summary");
-
-				//results Zip file is created
-				FileUtils.Zip(resultCatalogLocation,  pages, "Results.zip");
-
-				//redirects to results page (summary level)
-				RequestDispatcher rd = request.getRequestDispatcher(File.separator + sessionId + File.separator + "results" + File.separator + "Summary.html");
-				rd.forward(request, response);
+			//if there are tests available, set up the validator engine
+			boolean testsFound = false;
+			try {
+				testsFound = (getServletContext().getResourcePaths(testCatalogLocation).size() > 0);
+			} catch (Exception e) {
+				//a NullPointerException is thrown if the /WEB-INF/Tests directory is not found
+				e.printStackTrace();
 			}
+
+			//forwards to the error page if tests or /WEB-INF/Tests directory are missing 
+			if (!testsFound) {
+				request.setAttribute("ErrorMessage", "Tests not found.");
+				getServletContext().getRequestDispatcher("/error.jsp").forward(request, response);
+				return;
+			}
+
+			resultCatalogLocation = workDirectory + "results" + File.separator;
+
+			//setting up the validator engine with a repository and tests
+			ValidatorEngine engine = new ValidatorEngine();
+			engine.setRepository(repository);
+			engine.setStartTime(startTime);
+			engine.setResultCatalogLocation(resultCatalogLocation);
+
+			//adding tests to the validator engine
+			Set <String> testSuite = getServletContext().getResourcePaths(testCatalogLocation);
+			for (String testCase : testSuite) {
+				engine.addTest(getServletContext().getResourceAsStream(testCase));
+			}
+
+			//it's time to run all tests on this trimmed repository,
+			//save results in that location and time the whole operation
+			if (engine.ready()) {
+				engine.run();
+			}
+
+			//the results page is created
+			InputStream			xsl2html = null;
+			String				resultsFormat = (String) session.getAttribute("resultsFormat");
+			Map<String, String>	stylesheetParams = new HashMap<String, String> ();
+			String 				stylesheet = "";
+			String				errorsOnlyMode = "false";
+			File 				index = new File(resultCatalogLocation + "index.xml");
+
+			//setting up stylesheet parameters
+			stylesheetParams.put("SelectedSubjectArea", selectedSubjectArea);
+			if (resultsFormat.equals("ShowErrorsOnly")) {
+				errorsOnlyMode = "true";
+			}
+			stylesheetParams.put("ShowErrorsOnly", errorsOnlyMode);
+			stylesheetParams.put("SessionFolder", sessionId);
+
+			//creating both HTML pages (Summary and Details views)
+			Vector<String> pages = new Vector<String>();
+			pages.add("Summary");
+			pages.add("Compact");
+			pages.add("Details");
+			for (String page : pages) {
+				stylesheet = viewCatalogLocation + page + ".xsl";
+				xsl2html = getServletContext().getResourceAsStream(stylesheet);
+				XMLUtils.applyStylesheet(index, xsl2html, resultCatalogLocation + page + ".html", stylesheetParams);
+			}
+			//Summary contains links suitable to the Servlet Container environment
+			//No need to include it, it is replaced by Compact in the ZIP 
+			pages.remove("Summary");
+
+			//results Zip file is created
+			FileUtils.Zip(resultCatalogLocation,  pages, "Results.zip");
+
+			//redirects to results page (summary level)
+			RequestDispatcher rd = request.getRequestDispatcher(File.separator + sessionId + File.separator + "results" + File.separator + "Summary.html");
+			rd.forward(request, response);
 		}
 	}
 }

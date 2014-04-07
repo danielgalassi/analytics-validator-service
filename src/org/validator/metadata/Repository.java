2701dbdc532463a7e88d330f0584910a6cae30e8
danielgalassi@gmail.com <danielgalassi@gmail.com@b044f69f-097e-1059-c40c-61760cd1052f>
2014-04-07 05:26:30 +0000

    Small test.

--- src/org/validator/metadata/Repository.java
+++ src/org/validator/metadata/Repository.java
@@ -4,13 +4,19 @@
 package org.validator.metadata;
 
 import java.io.File;
-import java.util.Vector;
 
-import org.validator.utils.FileUtils;
-import org.validator.utils.SaxToDom;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
 import org.validator.utils.XMLUtils;
 import org.w3c.dom.Document;
-import org.xml.sax.XMLReader;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 
 /**
  * A basic class to handle the metadata repository file.
@@ -46,7 +52,11 @@ public class Repository {
 	 * @return true if the file if found and can be read
 	 */
 	public boolean available() {
-		return repository.exists() && repository.canRead();
+		boolean isAvailable = false;
+		if (repository != null) {
+			isAvailable = (repository.exists() && repository.canRead());
+		}
+		return isAvailable;
 	}
 
 	/**
@@ -54,17 +64,64 @@ public class Repository {
 	 * @param keepSubjectArea the name of the subject area to evaluate 
 	 */
 	public void trim(String keepSubjectArea) {
-		XMLReader		reader = FileUtils.getXMLReader();
-		SaxToDom		xml = new SaxToDom(null, reader, repository);
-		Vector<String>	subjectAreas = new Vector<String>();
 
-		subjectAreas.add(keepSubjectArea);
+		Document doc = XMLUtils.loadDocument(repository);
 
-		Document dom = xml.makeDom("PresentationCatalog",  subjectAreas);
-		XMLUtils.saveDocument(dom,  directory + File.separator + "metadata.xml");
-		//original file cleanup and file swap
+		XPathFactory xpathfactory = XPathFactory.newInstance();
+		XPath xpath = xpathfactory.newXPath();
+
+		try {
+			long start = System.currentTimeMillis();
+			XPathExpression expr = xpath.compile("//PresentationCatalog[@name='" + keepSubjectArea + "']");
+			Object result = expr.evaluate(doc, XPathConstants.NODESET);
+			NodeList presentationCatalog = (NodeList) result;
+			String myID = "";
+			for (int i = 0; i < presentationCatalog.item(0).getAttributes().getLength(); i++) {
+				NamedNodeMap y = presentationCatalog.item(0).getAttributes();
+				System.out.println(" -> id = " + y.getNamedItem("id"));
+				myID = y.getNamedItem("id").getNodeValue();
+				Node x = presentationCatalog.item(0).getAttributes().item(i);
+				System.out.println(x.getNodeName() + "\t" + x.getNodeValue());
+			}
+			expr = xpath.compile("//PresentationTable[@parentId='" + myID + "']");
+			result = expr.evaluate(doc, XPathConstants.NODESET);
+			NodeList presentationTable = (NodeList) result;
+
+			Document newMD = XMLUtils.createDOMDocument();
+	        Element repoTag = newMD.createElement("Repository");
+
+	        newMD.appendChild(repoTag);
+	        for (int i = 0; i < presentationCatalog.getLength(); i++) {
+	            Node node = presentationCatalog.item(i);
+	            Node copyNode = newMD.importNode(node, true);
+	            repoTag.appendChild(copyNode);
+	        }
+	        for (int i = 0; i < presentationTable.getLength(); i++) {
+	        	Node node = presentationTable.item(i);
+	        	Node copyNode = newMD.importNode(node, true);
+	        	repoTag.appendChild(copyNode);
+	        }
+	        XMLUtils.saveDocument(newMD,  directory + File.separator + "metadata.xml");
+	        System.out.println((System.currentTimeMillis() - start) / 1000);
+//			repository.delete();
+//			repository = null;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		repository.delete();
-		repository = new File (directory + File.separator + "metadata.xml");
+		repository = null;
+
+		//		XMLReader		reader = FileUtils.getXMLReader();
+		//		SaxToDom		xml = new SaxToDom(null, reader, repository);
+		//		Vector<String>	subjectAreas = new Vector<String>();
+		//
+		//		subjectAreas.add(keepSubjectArea);
+		//
+		//		Document dom = xml.makeDom("PresentationCatalog",  subjectAreas);
+		//		XMLUtils.saveDocument(dom,  directory + File.separator + "metadata.xml");
+		//		//original file cleanup and file swap
+		//		repository.delete();
+		//		repository = new File (directory + File.separator + "metadata.xml");
 	}
 
 	/**
